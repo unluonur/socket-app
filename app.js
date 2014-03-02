98443a785900aa5820c2ba44be426e7decc17f60
unluonur <unluonur@gmail.com>
2014-03-02 17:00:16 +0200

    re-organized project structure

---
+++ app.js
@@ -0,0 +1,4 @@
+var app = require('./lib/app');
+var config = require('./config.json');
+
+app(config.app);
--- cluster.js
+++ cluster.js
@@ -1,9 +1,9 @@
-var path = require('path');
 var cluster = require('cluster');
+var config = require('./config.json');
 var numCPUs = require('os').cpus().length;
 
 cluster.setupMaster({
-  exec: path.join(__dirname, 'app.js')
+  exec: 'app.js'
 });
 
 for (var i = 0; i < numCPUs; i++) {
@@ -13,3 +13,4 @@ for (var i = 0; i < numCPUs; i++) {
 cluster.on('exit', function(worker, code, signal) {
   console.log('worker ' + worker.process.pid + ' died');
 });
+
